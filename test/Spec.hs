5858b0e5342e78d662db91b8a3988255653a03ff
czechart <44610660+czechart@users.noreply.github.com>
2020-06-29 20:51:59 +0200

    Update Spec.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,11 @@
+import Lib
+import Test.Hspec
+import Test.QuickCheck
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Obliczanie przystosowania" $ do
+        it "zwraca 0 dla [0,0,0,0] i [0,0,0,0]" $
+            fitness [0,0,0,0] [0,0,0,0] `shouldBe` 0
+        it "zwraca 4 dla [0,0,0,0] i [1,1,1,1]" $
+            fitness [0,0,0,0] [1,1,1,1]`shouldBe` 4
